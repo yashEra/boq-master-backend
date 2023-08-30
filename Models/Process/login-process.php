77484b2afbfef354516d955a679571e965c7ccc4
Yasith Eranda Yapa Amarasekara <ya794120@gmail.com>
2023-08-30 13:07:01 +0530

    Update login-process.php

--- Models/Process/login-process.php
+++ Models/Process/login-process.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
@@ -18,23 +19,20 @@ $userName = $user['userName'];
 $password = $user['password'];
 
 switch ($method) {
-case 'POST':
-    $stmt = $conn->prepare("SELECT * FROM user WHERE userName = :userName;");
-    $stmt->bindParam(':userName', $userName);
-    $stmt->execute();
+    case 'POST':
+        $stmt = $conn->prepare("SELECT * FROM user WHERE userName = :userName;");
+        $stmt->bindParam(':userName', $userName);
+        // $stmt->bindParam(':password', $password);
+        $stmt->execute();
 
-    if ($stmt->rowCount() > 0) {
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        if (password_verify($password, $user['password'])) {
+        if ($stmt->rowCount() > 0) {
+            //header("Location: http://localhost:3000/signup");
             $response = ['success' => true];
         } else {
+            // Invalid username or password
             $response = ['success' => false];
         }
-    } else {
-        $response = ['success' => false];
-    }
 
-    echo json_encode($response);
-    break;
-
+        echo json_encode($response);
+        break;
 }
