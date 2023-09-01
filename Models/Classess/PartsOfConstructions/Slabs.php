253288eff190f1575a06204db58a49d451d3db92
sew-dulanji <100469312+sew-dulanji@users.noreply.github.com>
2023-08-31 17:38:15 -0700

    add slab process

--- Models/Classess/PartsOfConstructions/Slabs.php
+++ Models/Classess/PartsOfConstructions/Slabs.php
@@ -1,65 +1,73 @@
 <?php
 
-class Slab{
+class Slab
+{
     private $length; // length of the slab
     private $width; // width of the slab
-    private $height; // height of the slab
+    private $thickness; // thickness of the slab
     private $cement = 11; //11 50Kg bags for one cubic meter
-    private $sand = 15;//cubic feet for one cubic meter
-    private $metal = 30;// cubic feet for one cubic meter
-    private $reinforcementBars = 20.202;//Reinforcement bars square meter -1 for Kg
-    private $bindingWires = $reinforcementBars*0.01; //Binding wires square meter -1 for Kg
+    private $sand = 15; //cubic feet for one cubic meter
+    private $metal = 30; // cubic feet for one cubic meter
+    private $reinforcementBars = 20.202; //Reinforcement bars square meter -1 for Kg
+    private $bindingWires = $reinforcementBars * 0.01; //Binding wires square meter -1 for Kg
     private $numberOfSlabs;
 
-    public function __construct($length,$width,$height,$numberOfSlabs)
+    public function __construct($length, $width, $thickness, $numberOfSlabs)
     {
         $this->length = $length;
         $this->width = $width;
-        $this->height = $height;
+        $this->thickness = $thickness;
         $this->numberOfSlabs = $numberOfSlabs;
-        
     }
 
-    public function getVolOfSlab(){
-        $vol = $this->length*$this->width*$this->height; // in cubic meters
+    public function getVolOfSlab()
+    {
+        $vol = $this->length * $this->width * $this->thickness; // in cubic meters
         return $vol;
-    } 
+    }
 
-    public function getSqOfSlab(){
-        $sq = $this->length*$this->width; // in square meters
+    public function getSqOfSlab()
+    {
+        $sq = $this->length * $this->width; // in square meters
         return $sq;
-    } 
+    }
 
-    public function getCementPriceForSlab(){
-        $cementPrice = $this->cement* $this->getVolOfSlab();
+    public function getCementPriceForSlab()
+    {
+        $cementPrice = $this->cement * $this->getVolOfSlab();
         return $cementPrice;
     }
 
-    public function getSandPriceForSlab(){
-        $sandPrice = $this->sand* $this->getVolOfSlab();
+    public function getSandPriceForSlab()
+    {
+        $sandPrice = $this->sand * $this->getVolOfSlab();
         return $sandPrice;
     }
 
-    public function getMetalPriceForSlab(){
-        $metalPrice = $this->metal* $this->getVolOfSlab();
+    public function getMetalPriceForSlab()
+    {
+        $metalPrice = $this->metal * $this->getVolOfSlab();
         return $metalPrice;
     }
 
-    public function getReinforcementPriceForSlab(){
-        $reinforcementPrice = $this->reinforcementBars* $this->getSqOfSlab();
+    public function getReinforcementPriceForSlab()
+    {
+        $reinforcementPrice = $this->reinforcementBars * $this->getSqOfSlab();
         return $reinforcementPrice;
     }
 
-    public function getBindingWiresPriceForSlab(){
-        $bindingWiresPrice = $this->bindingWires* $this->getSqOfSlab();
+    public function getBindingWiresPriceForSlab()
+    {
+        $bindingWiresPrice = $this->bindingWires * $this->getSqOfSlab();
         return $bindingWiresPrice;
     }
 
-    public function getTotalCostForSlab(){
+    public function getTotalCostForSlab()
+    {
         $concreteCostOfSlab = $this->getCementPriceForSlab() + $this->getSandPriceForSlab() + $this->getMetalPriceForSlab(); // concrete cost for slab
         $rCostOfSlab = $this->getReinforcementPriceForSlab() + $this->getBindingWiresPriceForSlab(); //steels and wires cost for slab
 
-        $totalSlabCost = ($concreteCostOfSlab + $rCostOfSlab)*$this->numberOfSlabs;
+        $totalSlabCost = ($concreteCostOfSlab + $rCostOfSlab) * $this->numberOfSlabs;
         return $totalSlabCost;
     }
 }
